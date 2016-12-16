cb512ccc4221ada3b3976a9a2deb583ce9f04e0c
Alexis GIRARDI <alex205@users.noreply.github.com>
2016-12-16 11:53:48 +0100

    ajout test si nodjs

--- dodge_configure.sh
+++ dodge_configure.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-npm install
+node -v &> /dev/null
+if [ $? -ne 0 ]; then
 
-cd public/javascripts
-if [ ! -f socket.io.js ]; then
-  echo "Generating symbolic links"
-  ln -s ../../node_modules/socket.io-client/dist/socket.io.min.js socket.io.js
-fi
-cd ../..
+		echo "Nodejs not installed, abort"
 
-./dodge_make.sh
+else
+		npm install
 
-if [ ! -p img_queue ]
-then
-  echo "Create the named pipes"
-  mkfifo img_queue
+		cd public/javascripts
+		if [ ! -f socket.io.js ]; then
+				echo "Generating symbolic links"
+				ln -s ../../node_modules/socket.io-client/dist/socket.io.min.js socket.io.js
+		fi
+		cd ../..
+
+		./dodge_make.sh
+
+		if [ ! -p img_queue ]
+		then
+				echo "Create the named pipes"
+				mkfifo img_queue
+		fi
 fi
